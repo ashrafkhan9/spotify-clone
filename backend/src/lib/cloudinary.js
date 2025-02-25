1ec0f31933719cc2faea39b9eec8354a13a57ebe
Ashraf Khan <ashrafiim071@gmail.com>
2025-02-25 13:16:47 +0500

    initial commit

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -1,12 +1,29 @@
-import { v2 as cloudinary } from "cloudinary";
+// import { v2 as cloudinary } from "cloudinary";
 
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// cloudinary.config({
+// 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+// 	api_key: process.env.CLOUDINARY_API_KEY,
+// 	api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// export default cloudinary;
+
+import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
 dotenv.config();
 
+// Split CLOUDINARY_CONFIG into individual values
+const [CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET] =
+  process.env.CLOUDINARY_CONFIG.split(",");
+
+// Configure Cloudinary
 cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
+  CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
